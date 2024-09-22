cd0530a1e57a59ebeb66cbd2728d62eadef8e4cf
khan-rabiul <krabiulislam2@gmail.com>
2024-09-22 21:03:25 +0600

    Initial commit with Vite, React, Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+     <div className="text-5xl text-center text-pink-600 font-semibold font-mono mt-64 bg-slate-800 p-14 ">
+      Wellcome!!! React Tailwindcss Vite With Yarn Template
+     </div>
+    </>
+  )
+}
+
+export default App
